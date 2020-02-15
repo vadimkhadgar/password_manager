31cf35c8c44d60292e3760fed5fc9a09b016c7bc
Vadim Bliashuk <blyashuk@list.ru>
2020-02-15 12:16:36 +0300

    Created logic to Pin Activity.

--- app/src/main/java/com/vadimkor/passwordmanager/MainActivity.kt
+++ app/src/main/java/com/vadimkor/passwordmanager/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.vadimkor.passwordmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        finish()
     }
 }
